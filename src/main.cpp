7d321456bbc05e223901ffbd7f6a7ec0424a8b65
Jayden Campbell <jaydencampbell590@gmail.com>
2024-10-23 17:56:45 -0400

    Adding menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,75 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <limits>
+#include <algorithm>
 
 template<typename T>
 class StatColumn {
-    private:
+    public:
         std::string columnHeader;
         std::vector<T> records;  
-    public:
         StatColumn(std::string& columnHeader, std::vector<T>& records){
             this->columnHeader = columnHeader;
             this->records = records;
         }
-        void addRecord(T record){
-            this->records.push_back(record);
+        void addRecord(float record){
+            records.push_back(record);
         }
-        const auto getRecords() -> decltype(records){
-            return records;
+        decltype(auto) getRecords(){
+            return (records);
         }
 };
 
-int main(){
-    std::string h1 = "Weight";
-    std::vector<float> v1{};
-    StatColumn<float> s1{h1, v1};
-    s1.addRecord(114.1);
-    s1.addRecord(115.2);
-    std::vector<float> v2 = s1.getRecords();
-    for(int i = 0; i < v2.size(); i++){
-        std::cout << v2[i] << " ";
+struct MenuItem{
+    int id;
+    std::string description;
+};
+
+const std::vector<MenuItem> MENU_ITEMS{
+    {1, "Add Record"},
+    {2, "Display Current Records"},
+    {3, "Display Record Values"},
+    {0, "Exit the Program"}
+};
+
+void print_menu_items(std::vector<MenuItem> menuItems);
+
+bool id_in_menu(std::vector<MenuItem> menuItems, int id){
+    bool found = std::any_of(menuItems.begin(), menuItems.end(), [id](MenuItem item) { return id == item.id; });
+    return found;
+};
+
+int print_menu(std::vector<MenuItem> menuItems = MENU_ITEMS)
+{
+    int userInput;
+    print_menu_items(menuItems);
+
+    std::cin >> userInput;
+    while( !(std::cin.good() &&  id_in_menu(menuItems, userInput)) )
+{
+        std::cout << "Error: Invalid Console input. Please enter a valid number that is present in the menu." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // stop ignoring characters past the first newline encountered.
+        print_menu_items(menuItems);
+        std::cin >> userInput;
+    }
+
+    std::cout << "Valid entry accepted: " << userInput << std::endl;
+    return userInput;
+}
+
+void print_menu_items(std::vector<MenuItem> menuItems)
+{
+    for (MenuItem item : menuItems)
+    {
+        std::cout << item.id << ": " << item.description << std::endl;
     }
     std::cout << std::endl;
+};
+
+int main(){
+    int input = print_menu();
+    std::cout << "Option Selected: " << input << std::endl;
     return 0;
 }
